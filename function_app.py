d8a3e8dbc9173a09c00c6e594c284d3506b122f9
Ubuntu <venkat@vm.b5q3eha414qu3feuajiqbpbj0f.lx.internal.cloudapp.net>
2025-03-03 11:32:40 +0000

    added files

---
+++ function_app.py
@@ -0,0 +1,48 @@
+import azure.functions as func
+import datetime
+import json
+import logging
+name = "helloworld !!"
+app = func.FunctionApp()
+
+@app.route(route="MyFunction", auth_level=func.AuthLevel.ANONYMOUS)
+def MyFunction(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"{name}!!")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+
+@app.route(route="MyFunctionApp", auth_level=func.AuthLevel.ANONYMOUS)
+def MyFunctionApp(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"{name}")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
