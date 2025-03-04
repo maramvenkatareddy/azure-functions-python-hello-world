ae06accdbe816f3f97f0761511c286314ba09666
maramvenkatareddy <113689196+maramvenkatareddy@users.noreply.github.com>
2025-03-04 07:15:02 +0530

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,48 +1,12 @@
 import azure.functions as func
-import datetime
-import json
 import logging
+
 app = func.FunctionApp()
 
-@app.route(route="MyFunction", auth_level=func.AuthLevel.ANONYMOUS)
+@app.route(route="MyFunction", auth_level=func.AuthLevel.ANONYMOUS, methods=["GET"])
 def MyFunction(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    name = helloworld !!
+    logging.info("Python HTTP trigger function processed a request.")
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    name = "helloworld !!"  # Hardcoded Variable Here ✅
 
-    if name:
-        return func.HttpResponse(f"{name}!!")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
-
-@app.route(route="MyFunctionApp", auth_level=func.AuthLevel.ANONYMOUS)
-def MyFunctionApp(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    name = "helloworld !!"
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"{name}")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    return func.HttpResponse(f"{name}", status_code=200)
