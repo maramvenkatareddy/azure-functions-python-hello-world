6ff003dbb323ad9877b048cf923bb5ee5ed52033
maramvenkatareddy <113689196+maramvenkatareddy@users.noreply.github.com>
2025-03-04 07:20:14 +0530

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -7,6 +7,7 @@ app = func.FunctionApp()
 def MyFunction(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Python HTTP trigger function processed a request.")
 
-    name = "helloworld !!"  # Hardcoded Variable Here ✅
+    name = "helloworld !!"
+    html_content = f"<html><body><h1 style='color:red;'>{name}</h1></body></html>"
 
     return func.HttpResponse(f"{name}", status_code=200)
