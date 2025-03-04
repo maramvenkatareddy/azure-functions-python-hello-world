08a23e276c14792870dc32125bab8a47572279e3
maramvenkatareddy <113689196+maramvenkatareddy@users.noreply.github.com>
2025-03-04 10:25:54 +0530

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -19,3 +19,18 @@ def MyFunction(req: func.HttpRequest) -> func.HttpResponse:
     html_content = f"<html><body><h1 style='color:{random_color};'>{name}</h1></body></html>"
 
     return func.HttpResponse(html_content, status_code=200, mimetype="text/html")
+
+def venkat(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("Python HTTP trigger function processed a request.")
+
+    name = "I am Venkat"
+
+    # List of Colors 🔥
+    colors = ["red", "green", "blue", "orange", "purple", "yellow", "pink"]
+
+    # Pick Random Color
+    random_color = random.choice(colors)
+
+    html_content = f"<html><body><h1 style='color:{random_color};'>{name}</h1></body></html>"
+
+    return func.HttpResponse(html_content, status_code=200, mimetype="text/html")
