ef9cabe96b5ee95c0eea94ea89ade193df232f16
maramvenkatareddy <113689196+maramvenkatareddy@users.noreply.github.com>
2025-03-04 10:29:52 +0530

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -20,6 +20,7 @@ def MyFunction(req: func.HttpRequest) -> func.HttpResponse:
 
     return func.HttpResponse(html_content, status_code=200, mimetype="text/html")
 
+@app.route(route="venkat", auth_level=func.AuthLevel.ANONYMOUS, methods=["GET"])
 def venkat(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Python HTTP trigger function processed a request.")
 
